0e9315c8fd5853b34858ff9da9a7b3d28fddfd80
ekltn <tor-odin.ekle@arbetsformedlingen.se>
2024-12-04 21:38:26 +0100

    changed name

--- tutorial/__init__.py
+++ tutorial/__init__.py
@@ -3,7 +3,7 @@ from dagster import Definitions, load_assets_from_modules
 from . import assets
 from . import column_assets
 playground_group = load_assets_from_modules([assets], group_name="Playground")
-column_asset_group = load_assets_from_modules([column_assets], group_name="Column Assets")
+column_asset_group = load_assets_from_modules([column_assets], group_name="Column_Assets")
 all_assets = playground_group + column_asset_group
 
 defs = Definitions(
